b21d66d092b0e00f70627da7edcaea69ba2731aa
timmypass17 <timmypass17@gmail.com>
2025-07-07 23:43:17 -0700

    fix legend

--- FoodApp/ViewControllers/Home/CaloriesConsumedView.swift
+++ FoodApp/ViewControllers/Home/CaloriesConsumedView.swift
@@ -80,37 +80,63 @@ struct CaloriesConsumedView: View {
                 }
             }
             .padding(.bottom, 12)
+            
+            VStack(alignment: .leading, spacing: 8) {
+                Chart {
+                    ForEach(sortedMeals, id: \.self) { meal in
+                        BarMark(
+                            x: .value("Amount", min(meal.nutrientAmount(.calories), caloriesGoal)),
+                            y: .value("Type", "Calories"),
+                            height: 24
+                        )
+                        .foregroundStyle(by: .value("Meal", meal.name))
+                        .cornerRadius(4)
+                    }
 
-            Chart {
-                ForEach(sortedMeals, id: \.self) { meal in
-                    BarMark(
-                        x: .value("Amount", min(meal.nutrientAmount(.calories), caloriesGoal)),
-                        y: .value("Type", "Calories")
-                    )
-                    .foregroundStyle(by: .value("Meal", meal.name))
-                    .cornerRadius(4)
+                    if caloriesConsumed < caloriesGoal {
+                        BarMark(
+                            xStart: .value("Amount", 0),
+                            xEnd: .value("Goal", caloriesGoal),
+                            y: .value("Type", "Calories"),
+                            height: 24
+                        )
+                        .foregroundStyle(Color.gray.opacity(0.2))
+                        .cornerRadius(4)
+                    }
                 }
-                
-                if caloriesConsumed < caloriesGoal {
-                    BarMark(
-                        xStart: .value("Amount", 0),
-                        xEnd: .value("Goal", caloriesGoal),
-                        y: .value("Type", "Calories")
-                    )
-                    .foregroundStyle(Color.gray.opacity(0.2))
-                    .cornerRadius(4)
-                }
+                .frame(height: 50) // fixed chart height
+                .chartXScale(domain: 0...caloriesGoal)
+                .chartYAxis(.hidden)
+                .chartForegroundStyleScale(
+                    domain: sortedMealColorPairs.map { $0.0 },
+                    range: sortedMealColorPairs.map { $0.1 }
+                )
+                .chartLegend(.hidden)
+
+                // Have to make separate legend, messes up chart height when legend expands
+                TagCloudView(tags: sortedMealColorPairs)
             }
-//            .frame(height: 65)
-            .chartXScale(domain: 0...caloriesGoal)
-            .chartYAxis(.hidden)
-            .chartLegend(.visible)
-            .chartForegroundStyleScale(
-                domain: sortedMealColorPairs.map { $0.0 },
-                range: sortedMealColorPairs.map { $0.1 }
-            )
         }
         .padding(.vertical, 8)
+    }
+}
+
+struct WrapLegend: View {
+    let sortedMealColorPairs: [(String, Color)]
+
+    var body: some View {
+        // Auto-wrapping horizontal legend
+        LazyVGrid(columns: [], alignment: .leading, spacing: 8) {
+            ForEach(sortedMealColorPairs, id: \.0) { name, color in
+                HStack(spacing: 4) {
+                    Circle()
+                        .fill(color)
+                        .frame(width: 8, height: 8)
+                    Text(name)
+                        .font(.caption2)
+                }
+            }
+        }
     }
 }
 
@@ -128,3 +154,72 @@ extension Color {
         self.init(red: r, green: g, blue: b)
     }
 }
+
+struct TagCloudView: View {
+    var tags: [(String, Color)]
+
+    @State private var totalHeight
+          = CGFloat.zero       // << variant for ScrollView/List
+    //    = CGFloat.infinity   // << variant for VStack
+
+    var body: some View {
+        VStack {
+            GeometryReader { geometry in
+                self.generateContent(in: geometry)
+            }
+        }
+        .frame(height: totalHeight)// << variant for ScrollView/List
+        //.frame(maxHeight: totalHeight) // << variant for VStack
+    }
+
+    private func generateContent(in g: GeometryProxy) -> some View {
+        var width = CGFloat.zero
+        var height = CGFloat.zero
+
+        return ZStack(alignment: .topLeading) {
+            ForEach(self.tags, id: \.0) { tag in
+                HStack(spacing: 4) {
+                    Circle()
+                        .fill(tag.1)
+                        .frame(width: 8, height: 8)
+                    Text(tag.0)
+                        .font(.caption2)
+                        .foregroundStyle(.secondary)
+                }
+                .padding([.horizontal, .vertical], 4)
+                .alignmentGuide(.leading, computeValue: { d in
+                    if (abs(width - d.width) > g.size.width)
+                    {
+                        width = 0
+                        height -= d.height
+                    }
+                    let result = width
+                    if tag == self.tags.last! {
+                        width = 0 //last item
+                    } else {
+                        width -= d.width
+                    }
+                    return result
+                })
+                .alignmentGuide(.top, computeValue: {d in
+                    let result = height
+                    if tag == self.tags.last! {
+                        height = 0 // last item
+                    }
+                    return result
+                })
+            }
+        }.background(viewHeightReader($totalHeight))
+    }
+    
+    private func viewHeightReader(_ binding: Binding<CGFloat>) -> some View {
+        return GeometryReader { geometry -> Color in
+            let rect = geometry.frame(in: .local)
+            DispatchQueue.main.async {
+                binding.wrappedValue = rect.size.height
+            }
+            return .clear
+        }
+    }
+}
+
