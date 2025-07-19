1b5633593822d0d89b2153fee44fa0367c156dcf
timmypass17 <timmypass17@gmail.com>
2025-07-19 10:39:43 -0700

    deselect row

--- FoodApp/ViewControllers/FoodDetail/FoodDetailTableViewController.swift
+++ FoodApp/ViewControllers/FoodDetail/FoodDetailTableViewController.swift
@@ -256,6 +256,7 @@ class FoodDetailTableViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
         switch visibleSections[indexPath.section] {
         case .servingSize:
             if indexPath.row == 0 {
